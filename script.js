0e3a2f4aab445cf190843b1ec7447dd38659d6eb
Vikas Nallana <vikasnallana@gmail.com>
2024-12-01 21:57:39 +0530

    updated and checked

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+let imgbox = document.getElementById("imgbox");
+let qrimage = document.getElementById("qr-img");
+let qrtext = document.getElementById("qrtext");
 
+
+
+function generateQr(){
+
+    if(qrtext.value.length >0){
+        qrimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrtext.value;
+        imgbox.classList.add("show-img"); 
+    } 
+    else {
+        qrtext.classList.add('error');
+        setTimeout(()=>{
+            qrtext.classList.remove('error');
+        },1000)
+    } 
+}
